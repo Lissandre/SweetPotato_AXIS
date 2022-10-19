20e6afbe33ef04cc73bf02d1df0cf2106c6a5b60
Jean-Baptiste Sanchez <sanchez.jeanbapt@gmail.com>
2022-10-19 15:20:58 +0200

    Add starship class

---
+++ src/js/World/Starship.js
@@ -0,0 +1,28 @@
+import { TorusKnotGeometry, Mesh, MeshBasicMaterial, Object3D } from 'three'
+import gsap from 'gsap'
+
+export default class Starship {
+  constructor(options) {
+    // Options
+    this.assets = options.assets
+
+    // Set up
+    this.container = new Object3D()
+    this.container.name = 'Starship'
+
+    this.createStarship()
+    gsap.ticker.add((time, deltaTime) => {
+      this.setMovement(time, deltaTime)
+    })
+  }
+  createStarship() {
+    this.starship = new Mesh(
+      new TorusKnotGeometry(0.75, 1, 60, 6),
+      new MeshBasicMaterial({ color: 0xff0000 })
+    )
+    this.container.add(this.starship)
+  }
+  setMovement(time, deltaTime) {
+    this.starship.rotation.y += 0.001 * deltaTime
+  }
+}
